890d0ec46ee5c71b4212e133373b9e7adac6c2f0
AndresMorera17 <gmorera30808@ufide.ac.cr>
2025-12-08 18:13:59 -0600

    comments

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Motor de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(expressLayouts);
@@ -14,10 +13,8 @@ app.set('layout', 'layout');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// RUTAS (controladores)
 const inicioRouter  = require('./src/routes/inicio');
 const citasRouter   = require('./src/routes/citas');
 const clientesRouter = require('./src/routes/clientes');
@@ -35,7 +32,6 @@ app.get('/', (req, res) => {
   res.redirect('/cuenta/login');
 });
 
-// Rutas de la app
 app.use('/inicio', inicioRouter); 
 app.use('/citas', citasRouter);
 app.use('/clientes', clientesRouter);  
@@ -49,10 +45,6 @@ app.use('/proveedores', proveedoresRouter);
 app.use('/transporte', transporteRouter);
 
 
-
-
-
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`VetPost JS corriendo en http://localhost:${PORT}`);
 });
